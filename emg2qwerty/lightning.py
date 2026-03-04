25c3050710484eb7c589ea9b32b69e095a5b3c6a
KianBaghai <kianbaghai@g.ucla.edu>
2026-03-03 17:15:23 -0800

    more model fixes

--- emg2qwerty/lightning.py
+++ emg2qwerty/lightning.py
@@ -205,6 +205,13 @@ class TDSConvCTCModule(pl.LightningModule):
             lr_scheduler_config=self.lr_scheduler_config,
         )
 
+    def configure_optimizers(self) -> dict[str, Any]:
+        return utils.instantiate_optimizer_and_scheduler(
+            self.parameters(),
+            optimizer_config=self.optimizer_config,
+            lr_scheduler_config=self.lr_scheduler_config,
+        )
+
     def _step(
         self, phase: str, batch: dict[str, torch.Tensor], *args, **kwargs
     ) -> torch.Tensor:
